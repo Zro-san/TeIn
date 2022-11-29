7dbf556d1da746b22c0d1e694d4dfc0aab44f3ae
Zro <59265578+Zro-san@users.noreply.github.com>
2022-11-29 15:17:49 +0100

    Add files via upload

---
+++ Weihnachten.java
@@ -0,0 +1,19 @@
+
+public class Weihnachten {
+
+	public static void main(String[] args) {
+		
+	    for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5-1-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k < i*2+1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }   
+			
+	}
+			
+
